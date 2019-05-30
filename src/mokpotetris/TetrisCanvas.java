bf4a1453ade89fb77b905b9d48244e6fa0eb4fc2
shin6949 <shin6949@outlook.kr>
2019-05-30 14:17:55 +0900

    블록 추가

--- src/mokpotetris/TetrisCanvas.java
+++ src/mokpotetris/TetrisCanvas.java
@@ -58,8 +58,8 @@ public class TetrisCanvas extends JPanel implements Runnable, KeyListener {
 			for(int k = 0; k < TetrisData.COL; k++) {
 				if(data.getAt(i, k) == 0) {
 					g.setColor(colors[data.getAt(i, k)]);
-					g.draw3DRect(margin/2 + w * k, margin/2 + w * i, w, w, true); } 
-				else { 
+					g.draw3DRect(margin/2 + w * k, margin/2 + w * i, w, w, true); 
+					} else { 
 					g.setColor(colors[data.getAt(i, k)]);
 					g.fill3DRect(margin/2 + w * k, margin/2 + w * i, w, w, true);
 					}
