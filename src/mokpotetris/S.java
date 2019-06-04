98e42df959bcebe5b33e59d7c89db9f6ab68bbd4
hue412 <hue412@naver.com>
2019-06-04 14:21:48 +0900

    s추가

--- src/mokpotetris/S.java
+++ src/mokpotetris/S.java
@@ -4,14 +4,14 @@ public class S extends Piece {
 	
 	public S(TetrisData data) {
 		super(data);
-		c[0] = 1;	r[0] = 1;
-		c[1] = 1;	r[1] = 1;
-		c[2] = 1;	r[2] = 1;
-		c[3] = 1;	r[3] = 1;
+		c[0] = 1;	r[0] = 0;
+		c[1] = -1;	r[1] = 1;
+		c[2] = 0;	r[2] = 1;
+		c[3] = 0;	r[3] = 0;
 	}
 	
 	public int getType() {
-		return 4;
+		return 2;
 	}
 	
 	public int roteType() {
