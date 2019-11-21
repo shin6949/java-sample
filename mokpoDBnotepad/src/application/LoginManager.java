f999a30b8a45e49a03f0453572c7bd855ff8bb08
305 <cocoblue@kakao.com>
2019-11-21 16:33:51 +0900

    Login Scene Finished

--- mokpoDBnotepad/src/application/LoginManager.java
+++ mokpoDBnotepad/src/application/LoginManager.java
@@ -3,7 +3,7 @@ package application;
 public class LoginManager {
 	String logined_id;
 	
-	public static String DB_URL = "";
-	public static String DB_ID = "";
-	public static String DB_PW = "";
+	public static String DB_URL = "jdbc:mysql://52.231.157.32/notepad";
+	public static String DB_ID = "viewer";
+	public static String DB_PW = "mysqltest1234!";
 }
