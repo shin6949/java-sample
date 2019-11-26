e0cf5895d2676aade3c89bfc21d5d7938a9efd75
신재형 <cocoblue@kakao.com>
2019-11-27 03:47:01 +0900

    서버 정보 변경 (CentOS 7 -> Ubuntu, mysql -> maraidb)

--- mokpoDBnotepad/application/LoginManager.java
+++ mokpoDBnotepad/application/LoginManager.java
@@ -1,8 +1,10 @@
 package application;
 
 public class LoginManager {	
-	public static String DB_URL = "jdbc:mysql://52.231.157.32/notepad";
-	public static String DB_ID = "viewer";
+	//public static String DB_URL = "jdbc:mysql://52.231.157.32/notepad"; old
+	public static String DB_URL = "jdbc:mysql://52.231.156.211/notepad";
+	//public static String DB_ID = "viewer"; old
+	public static String DB_ID = "notepadviewer";
 	public static String DB_PW = "mysqltest1234!";
 
 }
